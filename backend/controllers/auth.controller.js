bb550122c82d105f75c74dafa168d4354d21f645
mowabanga <itsmundia@gmail.com>
2024-02-29 13:18:31 +0300

    Added logout API functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -52,8 +52,31 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  console.log("Login user");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const passwordCorrect = bcryptjs.compareSync(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || passwordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error logging in", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
