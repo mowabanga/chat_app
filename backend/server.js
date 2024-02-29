7252b68813d1e3d4435c0425fbccac383ba30068
mowabanga <itsmundia@gmail.com>
2024-02-29 16:13:24 +0300

    Implemented API for fetching sidebar users

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import ctMongoDB from "./db/ctMongoDB.js";
 
 const app = express();
@@ -13,8 +15,10 @@ dotenv.config();
 
 app.use(express.json()); // Parses incoming requests with JSON payloads(from req.body)
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 /*app.get("/", (req, res) => {
   res.send("Server is ready");
